d5b3a300f420542ab64e4542bd25cdd4d0b5702f
Christof <christofbless@gmail.com>
2017-04-01 19:07:05 +0200

    add comments

--- perceptron.py
+++ perceptron.py
@@ -22,9 +22,11 @@ class Perceptron:
         self.activation_function=kwargs.get("activation_function")
         
     def _add_bias(self,inputs):
+        """Add an additional constant 1 bias to the inputs to model the threshold"""
         return [(inp+[1],t) for inp,t in inputs]
         
     def train(self,inputs):
+        """update the weights of the modeled neuron iteratively based on some input data in the form (input_vector,desired_output)"""
         iterations=0
         # add the bias input
         if self.bias:
@@ -41,12 +43,13 @@ class Perceptron:
                     break                
             error_count=0
             for input_vector, desired_output in inputs: 
-                # sum of the product of input vector and weights compared against threshold = output
+                # sum of the dot product of input vector and weights compared against threshold = output
                 
                 if not self.activation_function:
-                    #step function
+                    #unit-step function
                     result= np.dot(input_vector, self.weights) > self.threshold
                 else:
+                    #custom activation function (use some sort of sigmoid error function)
                     result= self.activation_function(np.dot(input_vector, self.weights))
                 
                 # the error
@@ -69,20 +72,27 @@ class Perceptron:
             print "iterations: ",iterations
     
     def predict(self,data):
-        output=[]
-        if type(data)==list:
+        """predict instances based on the trained model"""
+        #check if we predict a list of vectors or just one vector
+        if type(data)==list and type(data[0]==list):
+            output=[]
+            if self.bias:
+                data=self._add_bias(data)
             for input_vector in test_points:
                 pred=np.dot(input_vector, self.weights) > self.threshold
                 output.append(pred)
+        else:
+            output = np.dot(data, self.weights) > self.threshold
         return output
             
                 
     def test(self,test_data):
+        """test the learned accuracy given on a test set of data"""
         if self.bias:
             test_data=self._add_bias(test_data)
         correct=0
         for input_vector, desired_output in test_data:
-            pred=np.dot(input_vector, self.weights) > self.threshold
+            pred=self.predict(input_vector)
             if pred==desired_output:
                 correct+=1
         print "accuracy: ", float(correct)/float(len(test_data))
@@ -91,6 +101,7 @@ class Perceptron:
     
 if __name__=="__main__":
     training_set_nand = [([0,0],1),([0,1],1),([1,0],1),([1,1],0)]
+    #xor with a third dimension x1*x2
     training_set_xor = [([0,0,0],0),([0,1,0],1),([1,0,0],1),([1,1,1],0)]
     p=Perceptron(verbose=True,max_iterations=100,activation_function=sigmoid)
     p.train(training_set_xor)
